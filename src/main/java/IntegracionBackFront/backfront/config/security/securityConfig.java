7ed03d920071cafe1ccd38b921d171bda744b1a2
BranAlec <20240208@ricaldone.edu.sv>
2025-09-01 15:11:55 -0600

    Cookies

--- src/main/java/IntegracionBackFront/backfront/config/security/securityConfig.java
+++ src/main/java/IntegracionBackFront/backfront/config/security/securityConfig.java
@@ -15,7 +15,7 @@ public class securityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         //Aqui van todos los endpoints publics que no requieren de un JWT
         http
-                .csrf(csrf -> csrf.disable());
+                .csrf(csrf -> csrf.disable())
 
     }
 
